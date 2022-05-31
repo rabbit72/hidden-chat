bfeeaceb4378fb4629a00c534e3116f56ffa1ff6
Daniil Shadrin <rabbit72rus@gmail.com>
2022-05-31 17:00:34 +0200

    Setup logging

--- src/writer.py
+++ src/writer.py
@@ -1,17 +1,47 @@
 import argparse
 import asyncio
+import logging
+
+
+def create_logger() -> logging.Logger:
+    logger = logging.getLogger("sender")
+    logger.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
+    ch.setFormatter(formatter)
+
+    logger.addHandler(ch)
+    return logger
+
+
+logger = create_logger()
 
 
 def send_message(writer, message: str):
-    writer.write(message.encode())
+    logger.debug(message.rstrip())
+    encoded_message = message.encode()
+    writer.write(encoded_message)
+
+
+async def read_message(reader) -> str:
+    message = await reader.readline()
+    decoded_message = message.decode()
+    logger.debug(decoded_message.rstrip())
+    return decoded_message
 
 
 async def main(address, port, user):
     message = "----------- Я СНОВА ТЕСТИРУЮ ЧАТИК.---------------"
     reader, writer = await asyncio.open_connection(address, port)
-
+    await read_message(reader)
     send_message(writer, user + "\n")
+    await read_message(reader)
+    await read_message(reader)
     send_message(writer, message + "\n\n")
+    await read_message(reader)
 
 
 if __name__ == "__main__":
